8f02a39353ac47b2796135399157cec6388c9dea
MateusLuisMk19 <95911492+MateusLuisMk19@users.noreply.github.com>
2025-11-12 17:00:42 +0000

    ff

---
+++ Notes.md
@@ -0,0 +1,20 @@
+instalar pacote no windows:
+py -m pip install nome_do_pacote
+
+criar ambiente virtual:
+py -m venv venv
+
+ativar ambiente virtual:
+venv\Scripts\activate
+
+desativar ambiente virtual:
+deactivate
+
+rodar o projeto:
+python main.py
+
+py -m pip install auto-py-to-exe
+
+pip show auto-py-to-exe
+
+auto-py-to-exe
--- main.py
+++ main.py
@@ -7,15 +7,7 @@ site1 = 'https://n8n-latest-8x9d.onrender.com'
 while True:
     try:
         resposta1 = requests.get(site1)
-        print(f'Site 1 ({site1}) Status: {resposta1.status_code}, Horário: {time.strftime("%Y-%m-%d %H:%M:%S")}')
+        print(f'({site1}) Status: {resposta1.status_code}, Horário: {time.strftime("%Y-%m-%d %H:%M:%S")}')
     except Exception as erro:
-        print(f'Erro na requisição do site 1: {erro}')
+        print(f'Erro na requisição do site: {erro}')
     time.sleep(30)
-
-    try:
-        time.sleep(10)
-        resposta2 = requests.get(site1)
-        print(f'Site 2 ({site1}) Status: {resposta2.status_code}, Horário: {time.strftime("%Y-%m-%d %H:%M:%S")}')
-    except Exception as erro:
-        print(f'Erro na requisição do site 1: {erro}')
-    time.sleep(35)
